21d803205effa52680149f03e6cb8a2ad3b5dcfd
Eugenia Antonova <136581152+EugeniaAntonova@users.noreply.github.com>
2023-11-03 09:45:47 +0600

    Update script.js
    update

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ const submit = form.querySelector('#submit-button');
 const success = document.querySelector('#succes-popup');
 const successCloseButton = success.querySelector('#success-button');
 const successBlanket = document.querySelector('.success');
+const userEmail = success.querySelector('.your-email');
 
 
 const isValidInput = (input) => {
     let inputValid = false;
-    if (input.value.match(/^[A-Za-z0-9\_\.]{1,}\@[A-Za-z0-9]{2,}\.[A-Za-z]{2,}$/)) {
+    if (input.match(/^[A-Za-z0-9\_\.]{1,}\@[A-Za-z0-9]{2,}\.[A-Za-z]{2,}$/)) {
         inputValid = true;
     };
     return inputValid;
@@ -23,7 +24,8 @@ const checkInput = (input) => {
     } else {
         errorMessage.classList.add('error');
         errorMessage.classList.remove('error--show');
-        submit.removeAttribute('disabled', 'true');
+        submit.removeAttribute('disabled');
+        submit.addEventListener('click', openSuccess)
     };
 }
 
@@ -48,20 +50,28 @@ const onEscKeydown = (evt) => {
 
 const openSuccess = () => {
     success.classList.add('success--show');
+    userEmail.textContent = input.value;
     document.addEventListener('keydown', onEscKeydown);
     successCloseButton.addEventListener('click', closeSuccess);
-    document.addEventListener('click', onBlanketClick)
-
+    document.addEventListener('click', onBlanketClick);
+    form.reset();
 }
 
-input.addEventListener ('change', () => {
+// i chose to use input instead of the change event, because validation is going more user friendly in this case. if i use change event, while focus 
+// stays in the input user can not see the result of fixing the value. only pressing enter/tab/clicking elsewhere makes error message dissapear. And
+// input event, on the other hand works beautifully, catching any changes and approving input while typing
+
+input.addEventListener ('input', () => {
     checkInput(input);
 })
-form.addEventListener('submit', (evt) => {
+
+// form.addEventListener('submit', (evt) => {
+// })
+
+// ----------i had to prevent submition, because i don`t know how to avoid default error messages, and preventDefault did not help. so i decided to catch
+// every click.
+
+submit.addEventListener('click', (evt) => {
     evt.preventDefault();
-    if (isValidInput(input)) {
-    openSuccess();
-    } else {
-    checkInput(input);    
-    }
+    checkInput(input);
 })
